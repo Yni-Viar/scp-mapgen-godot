6d6922fd764d01ba1011d79e486f05f53f5c4776
Yni-Viar <81305791+Yni-Viar@users.noreply.github.com>
2024-11-05 20:45:33 +0300

    MapGen v6 Released
    Added support for large rooms (like SCP-CB, no more using a hack!)

--- src/MapGen/MapGen.gd
+++ src/MapGen/MapGen.gd
@@ -8,20 +8,19 @@ enum RoomTypes {EMPTY, ROOM1, ROOM2, ROOM2C, ROOM3, ROOM4}
 @export var rng_seed: int = -1
 ## Rooms that will be used
 @export var rooms: Array[MapGenZone]
-# Unfinished, can lead to unconnected rooms
-#@export var generate_more_hallways: bool = false
-## EXPERIMENTAL. Enables large endrooms. Note, that this changes map generation behaviour.
-@export var enable_large_endrooms: bool = false
 ## Map size
 @export_range(8, 256, 2) var size: int = 8
 ## Room in grid size
 @export var grid_size: float = 20.48
 ## Amount of zones
 @export var zones_amount: int = 0
-#@export var large_room_support: bool = false
+## @experimental Large rooms support
+@export var large_rooms: bool = false
+## How much the map will be filled with rooms
 @export_range(0.25, 2) var room_amount: float = 0.75
 
 var mapgen: Array[Array] = []
+var disabled_points: Array[Vector2i] = []
 
 class Room:
 	# north, east, west and south check the connection between rooms.
@@ -84,42 +83,25 @@ func generate_zone_astar() -> void:
 			printerr("Too few rooms, map won't spawn")
 			return
 		# Available room position (for AStar walk)
-		var available_room_position: Array[Vector2] = [Vector2(0, size - 1),Vector2(0, size_y / (zones_amount + 1) * (zone_counter + 1) - 1)]
+		var available_room_position: Array[Vector2] = [Vector2(0, size - 1),Vector2(size_y / (zones_amount + 1) * zone_counter, size_y / (zones_amount + 1) * (zone_counter + 1) - 1)]
 		# Random room position. If large rooms enabled, also used for large room coordinates
 		var random_room: Vector2
-		# Large room module
-		if rooms[zone_counter].endrooms_single_large.size() > 0 && rooms[zone_counter].endrooms_single_large.size() <= 4 && enable_large_endrooms:
-			# Room, that connects large room and center of the map
-			var branch_position: Vector2
-			# If there is a large room, decrease available size
-			for k in range(rooms[zone_counter].endrooms_single_large.size()):
-				match k:
-					0:
-						random_room = Vector2(0, 0)
-						branch_position = Vector2(1, zone_center)
-						available_room_position[0] = Vector2(2, size - 1)
-						available_room_position[1] = Vector2(2, size_y / (zones_amount + 1) * (zone_counter + 1) - 1)
-					1:
-						random_room = Vector2(0, size_y / (zones_amount + 1) * (zone_counter + 1) - 2)
-						available_room_position[1] = Vector2(2, size_y / (zones_amount + 1) * (zone_counter + 1) - 2)
-					2:
-						random_room = Vector2(size - 1, 1)
-						branch_position = Vector2(size-1, zone_center)
-						available_room_position[0] = Vector2(2, size - 2)
-					3:
-						random_room = Vector2(size - 3, size_y / (zones_amount + 1) * (zone_counter + 1) - 2)
-				mapgen[random_room.x][random_room.y].large = true
-				walk_astar(Vector2(temp_x, temp_y), branch_position)
-				walk_astar(branch_position, random_room)
-		elif rooms[zone_counter].endrooms_single_large.size() > 4:
-			printerr("Invalid amount of large rooms, map won't spawn. You should set in zone's large_rooms_amount only values between 0 and 4 (inclusive).")
-			return
-		# Walk before need-to-spawn rooms runs out
+		## Reworked large rooms module
+		if large_rooms && rooms[zone_counter].endrooms_single_large.size() > 0:
+			var large_room_amount = size / 6
+			for i in range(large_room_amount):
+				while true:
+					random_room = Vector2(rng.randi_range(available_room_position[0].x, available_room_position[0].y), rng.randi_range(available_room_position[1].x, available_room_position[1].y))
+					if check_room_dimensions(random_room.x, random_room.y):
+						walk_astar(Vector2(temp_x, temp_y), random_room)
+						mapgen[random_room.x][random_room.y].large = true
+						break
+		## Walk before need-to-spawn rooms runs out
 		while number_of_rooms > 0:
 			random_room = Vector2(rng.randi_range(available_room_position[0].x, available_room_position[0].y), rng.randi_range(available_room_position[1].x, available_room_position[1].y))
 			walk_astar(Vector2(temp_x, temp_y), random_room)
 			number_of_rooms -= 1
-		# Connect two zones
+		## Connect two zones
 		if zone_counter < zones_amount:
 			var tmp_2: float
 			if (zone_counter % 2 == 1):
@@ -130,24 +112,162 @@ func generate_zone_astar() -> void:
 			walk_astar(Vector2(temp_x, temp_y), Vector2(temp_x, roundi(zone_center)))
 		zone_counter += 1
 	place_room_positions()
-## for future map customization
+# not used
 #func customize_room_connections():
-	#for i in range(1, size - 1):
-		#for j in range(1, size_y - 1):
-			#if generate_more_hallways:
-				#if mapgen[i][j].north && mapgen[i][j].east && mapgen[i][j].west && !mapgen[i][j].south && mapgen[i - 1][j].south && mapgen[i-1][j].east && mapgen[i-1][j].west && !mapgen[i-1][j].north:
-						#mapgen[i][j].north = false
-						#mapgen[i - 1][j].south = false
-				#if mapgen[i][j].south && mapgen[i][j].east && mapgen[i][j].west && !mapgen[i][j].north && mapgen[i + 1][j].north && mapgen[i+1][j].east && mapgen[i+1][j].west && !mapgen[i+1][j].south:
-						#mapgen[i][j].south = false
-						#mapgen[i + 1][j].north = false
-				#if mapgen[i][j].north && !mapgen[i][j].east && mapgen[i][j].west && mapgen[i][j].south && mapgen[i - 1][j].south && mapgen[i-1][j].east && !mapgen[i-1][j].west && mapgen[i-1][j].north:
-						#mapgen[i][j].west = false
-						#mapgen[i - 1][j].east = false
-				#if mapgen[i][j].north && mapgen[i][j].east && !mapgen[i][j].west && mapgen[i][j].south && mapgen[i + 1][j].south && !mapgen[i+1][j].east && mapgen[i+1][j].west && mapgen[i+1][j].north:
-						#mapgen[i][j].east = false
-						#mapgen[i + 1][j].west = false
+	#
 	#place_room_positions()
+## checks spawn places for large rooms
+func check_room_dimensions(x: int, y: int) -> bool:
+	if x == 0 && y == 0:
+		if !mapgen[x + 1][y].exist:
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		elif !mapgen[x][y + 1].exist:
+			disabled_points.append(Vector2i(x, y + 1))
+			return true
+		else:
+			return false
+	elif x == size - 1 && y == size_y - 1:
+		if !mapgen[x - 1][y].exist:
+			disabled_points.append(Vector2i(x - 1, y))
+			return true
+		elif !mapgen[x][y - 1].exist:
+			disabled_points.append(Vector2i(x, y - 1))
+			return true
+		else:
+			return false
+	elif x == 0 && y == size_y - 1:
+		if !mapgen[x + 1][y].exist:
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		elif !mapgen[x][y - 1].exist:
+			disabled_points.append(Vector2i(x, y - 1))
+			return true
+		else:
+			return false
+	elif x == size - 1 && y == 0:
+		if !mapgen[x - 1][y].exist:
+			disabled_points.append(Vector2i(x - 1, y))
+			return true
+		elif !mapgen[x][y + 1].exist:
+			disabled_points.append(Vector2i(x, y + 1))
+			return true
+		else:
+			return false
+	## |[x][x]  |        |[x]
+	## |[o][x]  |[o][x]  |[o]
+	## |        |[x][x]  |[x]
+	elif x == 0:
+		if !mapgen[x][y + 1].exist && !mapgen[x + 1][y + 1].exist && !mapgen[x + 1][y].exist: 
+			disabled_points.append(Vector2i(x, y + 1))
+			disabled_points.append(Vector2i(x + 1, y + 1))
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		elif !mapgen[x][y - 1].exist && !mapgen[x + 1][y - 1].exist && !mapgen[x + 1][y].exist:
+			disabled_points.append(Vector2i(x, y - 1))
+			disabled_points.append(Vector2i(x + 1, y - 1))
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		elif !mapgen[x][y + 1].exist && !mapgen[x][y - 1].exist:
+			disabled_points.append(Vector2i(x, y + 1))
+			disabled_points.append(Vector2i(x, y - 1))
+			return true
+		else:
+			return false
+	## [x][x]|        |  [x]|
+	## [x][o]|  [x][o]|  [o]|
+	##       |  [x][x]|  [x]|
+	elif x == size - 1:
+		if !mapgen[x][y + 1].exist && !mapgen[x - 1][y + 1].exist && !mapgen[x - 1][y].exist:
+			disabled_points.append(Vector2i(x, y + 1))
+			disabled_points.append(Vector2i(x - 1, y + 1))
+			disabled_points.append(Vector2i(x - 1, y))
+			return true
+		elif !mapgen[x][y - 1].exist && !mapgen[x - 1][y - 1].exist && !mapgen[x - 1][y].exist:
+			disabled_points.append(Vector2i(x, y - 1))
+			disabled_points.append(Vector2i(x - 1, y - 1))
+			disabled_points.append(Vector2i(x - 1, y))
+			return true
+		elif !mapgen[x][y + 1].exist && !mapgen[x][y - 1].exist:
+			disabled_points.append(Vector2i(x, y + 1))
+			disabled_points.append(Vector2i(x, y - 1))
+			return true
+		else:
+			return false
+	## [x][x]   [x][x]   
+	## [o][x]   [x][o]   [x][o][x]
+	## ------   ------   ---------
+	elif y == 0:
+		if !mapgen[x][y + 1].exist && !mapgen[x + 1][y + 1].exist && !mapgen[x + 1][y].exist:
+			disabled_points.append(Vector2i(x, y + 1))
+			disabled_points.append(Vector2i(x + 1, y + 1))
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		elif !mapgen[x - 1][y].exist && !mapgen[x - 1][y + 1].exist && !mapgen[x][y + 1].exist:
+			disabled_points.append(Vector2i(x - 1, y))
+			disabled_points.append(Vector2i(x - 1, y + 1))
+			disabled_points.append(Vector2i(x, y + 1))
+			return true
+		elif !mapgen[x + 1][y].exist && !mapgen[x - 1][y].exist:
+			disabled_points.append(Vector2i(x - 1, y))
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		else:
+			return false
+	## ------   ------   ---------  
+	## [o][x]   [x][o]   [x][o][x]
+	## [x][x]   [x][x]
+	elif y == size_y - 1:
+		if !mapgen[x + 1][y].exist && !mapgen[x + 1][y - 1].exist && !mapgen[x][y - 1].exist:
+			disabled_points.append(Vector2i(x + 1, y))
+			disabled_points.append(Vector2i(x + 1, y - 1))
+			disabled_points.append(Vector2i(x, y - 1))
+			return true
+		elif !mapgen[x - 1][y].exist && !mapgen[x - 1][y - 1].exist && !mapgen[x][y - 1].exist:
+			disabled_points.append(Vector2i(x - 1, y))
+			disabled_points.append(Vector2i(x - 1, y - 1))
+			disabled_points.append(Vector2i(x, y - 1))
+			return true
+		elif !mapgen[x + 1][y].exist && !mapgen[x - 1][y].exist:
+			disabled_points.append(Vector2i(x - 1, y))
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		else:
+			return false
+	## [x][x]   [x][x]   [x][x][x]
+	## [x][o]   [o][x]   [x][o][x]   [x][o][x]
+	## [x][x]   [x][x]               [x][x][x]
+	else:
+		if !mapgen[x][y + 1].exist && !mapgen[x - 1][y + 1].exist && !mapgen[x - 1][y - 1].exist && !mapgen[x][y - 1].exist && !mapgen[x - 1][y].exist:
+			disabled_points.append(Vector2i(x, y + 1))
+			disabled_points.append(Vector2i(x - 1, y + 1))
+			disabled_points.append(Vector2i(x - 1, y - 1))
+			disabled_points.append(Vector2i(x, y - 1))
+			disabled_points.append(Vector2i(x - 1, y))
+			return true
+		elif !mapgen[x][y + 1].exist && !mapgen[x + 1][y + 1].exist && !mapgen[x + 1][y - 1].exist && !mapgen[x][y - 1].exist && !mapgen[x + 1][y].exist:
+			disabled_points.append(Vector2i(x, y + 1))
+			disabled_points.append(Vector2i(x + 1, y + 1))
+			disabled_points.append(Vector2i(x + 1, y - 1))
+			disabled_points.append(Vector2i(x, y - 1))
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		elif !mapgen[x - 1][y].exist && !mapgen[x - 1][y + 1].exist && !mapgen[x][y + 1].exist && !mapgen[x + 1][y + 1].exist  && !mapgen[x + 1][y].exist:
+			disabled_points.append(Vector2i(x - 1, y))
+			disabled_points.append(Vector2i(x - 1, y + 1))
+			disabled_points.append(Vector2i(x, y + 1))
+			disabled_points.append(Vector2i(x + 1, y + 1))
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		elif !mapgen[x - 1][y].exist && !mapgen[x - 1][y - 1].exist && !mapgen[x][y - 1].exist && !mapgen[x + 1][y - 1].exist  && !mapgen[x + 1][y].exist:
+			disabled_points.append(Vector2i(x - 1, y))
+			disabled_points.append(Vector2i(x - 1, y - 1))
+			disabled_points.append(Vector2i(x, y - 1))
+			disabled_points.append(Vector2i(x + 1, y - 1))
+			disabled_points.append(Vector2i(x + 1, y))
+			return true
+		else:
+			return false
 
 ## Main walker function, using AStarGrid2D
 func walk_astar(from: Vector2, to: Vector2) -> void:
@@ -157,6 +277,8 @@ func walk_astar(from: Vector2, to: Vector2) -> void:
 	astar_grid.cell_size = Vector2(1, 1)
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	astar_grid.update()
+	for obstacle in disabled_points:
+		astar_grid.set_point_solid(obstacle)
 	var previous_map: Vector2 = from
 	# Walk
 	for map in astar_grid.get_point_path(from, to):
@@ -191,11 +313,12 @@ func place_room_positions() -> void:
 			print(int(mapgen[j][k].exist))
 		print()
 	
-	var room1_amount: int = 0
-	var room2_amount: int = 0
-	var room2c_amount: int = 0
-	var room3_amount: int = 0
-	var room4_amount: int = 0
+	# deprecated since 6.0, copied from pre-5.0 map generators
+	#var room1_amount: int = 0
+	#var room2_amount: int = 0
+	#var room2c_amount: int = 0
+	#var room3_amount: int = 0
+	#var room4_amount: int = 0
 	
 	for l in range(size):
 		for m in range(size_y):
@@ -214,82 +337,82 @@ func place_room_positions() -> void:
 						var room_angle: Array[float] = [0, 90, 180, 270]
 						mapgen[l][m].room_type = RoomTypes.ROOM4
 						mapgen[l][m].angle = room_angle[rng.randi_range(0, 3)]
-						room4_amount += 1
+						#room4_amount += 1
 					elif east && !west:
 						#room3, pointing east
 						mapgen[l][m].room_type = RoomTypes.ROOM3
 						mapgen[l][m].angle = 90
-						room3_amount += 1
+						#room3_amount += 1
 					elif !east && west:
 						#room3, pointing west
 						mapgen[l][m].room_type = RoomTypes.ROOM3
 						mapgen[l][m].angle = 270
-						room3_amount += 1
+						#room3_amount += 1
 					else:
 						#vertical room2
 						var room_angle: Array[float] = [0, 180]
 						mapgen[l][m].room_type = RoomTypes.ROOM2
 						mapgen[l][m].angle = room_angle[rng.randi_range(0, 1)]
-						room2_amount += 1
+						#room2_amount += 1
 				elif east && west:
 					if north && !south:
 						#room3, pointing north
 						mapgen[l][m].room_type = RoomTypes.ROOM3
 						mapgen[l][m].angle = 0
-						room3_amount += 1
+						#room3_amount += 1
 					elif !north && south:
 					#room3, pointing south
 						mapgen[l][m].room_type = RoomTypes.ROOM3
 						mapgen[l][m].angle = 180
-						room3_amount += 1
+						#room3_amount += 1
 					else:
 					#horizontal room2
 						var room_angle: Array[float] = [90, 270]
 						mapgen[l][m].room_type = RoomTypes.ROOM2
 						mapgen[l][m].angle = room_angle[rng.randi_range(0, 1)]
-						room2_amount += 1
+						#room2_amount += 1
 				elif north:
 					if east:
 					#room2c, north-east
 						mapgen[l][m].room_type = RoomTypes.ROOM2C
 						mapgen[l][m].angle = 0
-						room2c_amount += 1
+						#room2c_amount += 1
 					elif west:
 					#room2c, north-west
 						mapgen[l][m].room_type = RoomTypes.ROOM2C
 						mapgen[l][m].angle = 270
-						room2c_amount += 1
+						#room2c_amount += 1
 					else:
 					#room1, north
 						mapgen[l][m].room_type = RoomTypes.ROOM1
 						mapgen[l][m].angle = 0
-						room1_amount += 1
+						#room1_amount += 1
 				elif south:
 					if east:
 					#room2c, south-east
 						mapgen[l][m].room_type = RoomTypes.ROOM2C
 						mapgen[l][m].angle = 90
-						room2c_amount += 1
+						#room2c_amount += 1
 					elif west:
 					#room2c, south-west
 						mapgen[l][m].room_type = RoomTypes.ROOM2C
 						mapgen[l][m].angle = 180
-						room2c_amount += 1
+						#room2c_amount += 1
 					else:
 					#room1, south
 						mapgen[l][m].room_type = RoomTypes.ROOM1
 						mapgen[l][m].angle = 180
-						room1_amount += 1
+						#room1_amount += 1
 				elif east:
 					#room1, east
 					mapgen[l][m].room_type = RoomTypes.ROOM1
 					mapgen[l][m].angle = 90
-					room1_amount += 1
+					#room1_amount += 1
 				else:
 					#room1, west
 					mapgen[l][m].room_type = RoomTypes.ROOM1
 					mapgen[l][m].angle = 270
-					room1_amount += 1
+					#room1_amount += 1
 	spawn_rooms()
 
 ## Spawns room prefab on the grid
@@ -321,11 +444,11 @@ func spawn_rooms() -> void:
 			var room: StaticBody3D
 			match mapgen[n][o].room_type:
 				RoomTypes.ROOM1:
-					if mapgen[n][o].large && enable_large_endrooms:
+					if mapgen[n][o].large && large_rooms && rooms[zone_counter].endrooms_single_large.size() > 0:
 						selected_room = rooms[zone_counter].endrooms_single_large[large_room_counter[zone_counter]].prefab
 						large_room_counter[zone_counter] += 1
 					else:
-						if rooms[zone_counter].endrooms_single_large.size() > 0 && !enable_large_endrooms && large_room_counter[zone_counter] < rooms[zone_counter].endrooms_single_large.size():
+						if rooms[zone_counter].endrooms_single_large.size() > 0 && !large_rooms && large_room_counter[zone_counter] < rooms[zone_counter].endrooms_single_large.size():
 							selected_room = rooms[zone_counter].endrooms_single_large[large_room_counter[zone_counter]].prefab
 							large_room_counter[zone_counter] += 1
 						elif (room1_count[zone_counter] >= rooms[zone_counter].endrooms_single.size()):
@@ -479,7 +602,8 @@ func spawn_doors():
 		zone_counter = 0
 ## Clears the map generation
 func clear():
-	mapgen = []
+	disabled_points.clear()
+	mapgen.clear()
 	for node in get_children():
 		node.queue_free()
 
